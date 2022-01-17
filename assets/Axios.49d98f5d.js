05def78afc39c487dc37386f3b85062eef890212
TEABOY-WEB <TEABOY-WEB@users.noreply.github.com>
2022-01-17 02:52:10 +0000

    Update Vite2.x + Vue3.x + TypeScript Starter 342a63b8e1475e7ae41384fd8fedeb98fb8d6d8d

---
+++ assets/Axios.49d98f5d.js
@@ -0,0 +1,1 @@
+import{d as a,a as o,o as s,b as t,t as n}from"./vendor.5a54200c.js";const i=a({setup(r){const e=o("axios");return(_,l)=>(s(),t("div",null,n(e.value.value),1))}});export{i as default};
